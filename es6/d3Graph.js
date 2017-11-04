381fdb5d1932a63da8b26d8acfdd6a243c05f132
Jeff <jeff@Jeffs-MacBook-Pro.local>
2017-11-04 15:08:43 -0500

    Initial Commit. Adds D3 Graph based on mousepos

--- es6/d3Graph.js
+++ es6/d3Graph.js
@@ -1,15 +1,40 @@
-import d3 from 'd3'
+import d3 from 'd3';
+import Rx from 'rxjs';
 
 export default class d3Graph {
-    constructor(id) {
+    constructor(id, samples) {
         this.container = d3.select(id).append('svg').append('g');
-    }
-    drawRectAngle()	{
-    	this.container.append('rect')
-                            .attr('x', 10)
-                            .attr('y', 10)
-                            .attr('width', 50)
-                            .attr('height', 100);
+        this.samples = samples;
+        this.data = [];
+        this.windowWidth = //                          Minus Padding
+            document.getElementById('wrapper').offsetWidth - 20 - samples * 2;
+
+        var keyups = Rx.Observable.fromEvent(document, 'mousemove')
+            .map(x => ({x: x.clientX , y : x.clientY}))
+            .take(this.samples)
+            .scan((acc, cur) => [...acc ,cur],[])
+            .subscribe(x => {
+                this.data = x;
+                console.log("X", x);
+                let inc  = (255/this.data.length);
+                d3.select(".chart2")
+                    .selectAll("div")
+                    .data(this.data)
+                    .enter()
+                    .append("div")
+                    .style("height", function(d,i) { return d.y + "px"; })
+                    .style('background-color' , function(d,i){
+                        let indexInc = Math.floor(i * inc);
+                        return `rgb(0, 0, ${indexInc})`;
+                    })
+                    .style('width', (d,i) => {
+                        console.log(this.windowWidth / this.data.length);
+                        return this.windowWidth / this.samples + 'px';
+                    })
+
+                    .text(function(d) { return d.y; });
+            });
+
     }
 
 }
--- es6/main.js
+++ es6/main.js
@@ -1,3 +1,2 @@
 import d3Graph from './d3Graph.js';
-let graph = new d3Graph('#graph');
-graph.drawRectAngle();
+let graph = new d3Graph('#chart2', 200);
